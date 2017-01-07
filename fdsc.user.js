feeb48f9aefa0770b3d31bd5580865d7da4b2359
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-01-07 13:07:36 +0100

    Version bump

--- fdsc.user.js
+++ fdsc.user.js
@@ -6,7 +6,7 @@
 // @contributor angussidney
 // @contributor rene
 // @attribution Brock Adams (https://github.com/BrockA)
-// @version     1.10.1
+// @version     1.10.2
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fdsc.user.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fdsc.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
