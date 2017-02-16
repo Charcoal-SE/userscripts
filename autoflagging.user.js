88cccc99d2f1e89cc7cc3b960c7f70398d3a3926
Jed Fox <git@twopointzero.us>
2017-02-16 06:35:03 -0500

    Fix Flagged By info.

--- autoflagging.user.js
+++ autoflagging.user.js
@@ -159,15 +159,13 @@
     if (!$autoflag.find(".ai-flag-count").length) {
       $autoflag.append($("<span/>").addClass("ai-flag-count"));
     }
-    if ($autoflag.data("users")) {
-      data.users = $autoflag.data("users").concat(data.users)
-    }
+
     $autoflag.find(".ai-you-flagged").toggle(data.flagged && data.youFlagged);
     $autoflag.find(".ai-flag-count")
              .text(data.flagged ? "" + data.users.length : "")
              .toggleClass("ai-not-autoflagged", !data.flagged)
              .attr("title", data.flagged
-               ? "Flagged by " + data.users.map(function (user) { return user.username }).join(", ")
+               ? "Flagged by " + data.users.map(function (user) { return user.username || user.user_name }).join(", ")
                : "Not Autoflagged");
     $autoflag.data("users", data.users)
   };
