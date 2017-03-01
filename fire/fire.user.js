53b9562a3d7dd64d721201eac2bce3a4f60a3d32
Cerbrus <michiel1900@gmail.com>
2017-03-01 13:21:22 +0100

    Reorganize, fix button floating

--- fire/fire.user.js
+++ fire/fire.user.js
@@ -18,7 +18,11 @@
 (function () {
   "use strict";
 
-  if (!autoflagging) {
+  if (autoflagging) {
+    // Plugin registration
+    autoflagging.decorate.fire = onSmokeDetectorReport;
+    autoflagging.decorate.fire.location = "after";
+  } else {
     console.error("FIRE dependency missing: The AIM userscript isn't loaded.");
     console.warn("AIM GitHub url: https://github.com/Charcoal-SE/userscripts/blob/master/autoflagging.user.js");
     console.warn("AIM Download url: https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging.user.js");
@@ -26,31 +30,29 @@
   }
 
   var wip = true;
+  injectCSS(wip);
 
-  if (wip) {
-    // This userscript is still work in progress. It's probably broken, useless or otherwise dangerous.
-    $.get(
-      "https://api.github.com/repos/Charcoal-SE/Userscripts/commits/FIRE",
-      function (commitData) {
-        var css = window.document.createElement("link");
-        css.rel = "stylesheet";
-        css.href = "https://cdn.rawgit.com/Charcoal-SE/Userscripts/" + commitData.sha + "/fire/fire.css";
-        document.head.appendChild(css);
-      }
-    );
-  } else {
-    // Release
-    // Inject CSS
-    var css = window.document.createElement("link");
-    css.rel = "stylesheet";
-    css.href = "//charcoal-se.org/userscripts/fire.css"; // "cdn.rawgit.com/Charcoal-SE/userscripts/master/fire/fire.css"
-    document.head.appendChild(css);
+  // Smoke Detector Report handler
+  function onSmokeDetectorReport($fire, data) {
+    if ($fire.find(".ai-fire-button").length === 0) {
+      var fireButton = element("span", "ai-fire-button", {
+        text: "🛠️",
+        click: function () {
+          data.is_answer = data.link.indexOf("/a/") >= 0; // eslint-disable-line camelcase
+          openPopup(data);
+        }
+      });
+
+      $fire.prepend(fireButton);
+    }
   }
 
+  // Popup methods
   function closePopup() {
-    $(".ai-fire-popup").fadeOut("fast", function () {
-      $(this).remove();
-    });
+    $(".ai-fire-popup")
+      .fadeOut("fast", function () {
+        $(this).remove();
+      });
   }
 
   function closePopupOnEsc(e) {
@@ -64,35 +66,20 @@
     var w = window.innerWidth / 2;
     var d = data;
 
-    var popup = $("<div />", {class: "ai-fire-popup"})
+    var popup = element("div", "ai-fire-popup")
       .css({top: 100, left: w - 600});
 
-    var tpuButton = $("<a />", {
-      class: "button",
-      text: "tpu-",
-      click: function () {
-        feedback(d, "tpu-");
-      }
-    });
-    var fpButton = $("<a />", {
-      class: "button",
-      text: "fp",
-      click: function () {
-        feedback(d, "fp");
-      }
-    });
-    var closeButton = $("<a />", {
-      class: "button ai-fire-close-button",
+    var closeButton = element("a", "button ai-fire-close-button", {
       text: "Close",
       click: closePopup
     });
 
-    var top = $("<p/>", {class: "ai-fire-popup-header"})
-      .append(tpuButton)
-      .append(fpButton)
+    var top = element("p", "ai-fire-popup-header")
+      .append(createFeedbackButton(d, "tpu-", "tpu-"))
+      .append(createFeedbackButton(d, "fp", "fp"))
       .append(closeButton);
 
-    var body = $("<div/>", {class: "ai-fire-popup-body"})
+    var body = element("div", "ai-fire-popup-body")
       .append($("<h3 />", {text: d.title}))
       .append($("<hr />"))
       .append(d.body);
@@ -107,6 +94,7 @@
     $(document).keyup(closePopupOnEsc);
   }
 
+  // Provide feedback / flag
   function feedback(data, verdict) {
     // if (data.autoflagged && data.autoflagged.flagged) {
     //   // Only allow actual flagging if this has been flagged already.
@@ -115,20 +103,43 @@
     closePopup();
   }
 
-  autoflagging.decorate.fire = function ($fire, data) {
-    if ($fire.find(".ai-fire-button").length === 0) {
-      var fireButton = $("<span/>", {
-        class: "ai-fire-button",
-        text: "🛠️",
-        click: function () {
-          data.is_answer = data.link.indexOf("/a/") >= 0; // eslint-disable-line camelcase
-          openPopup(data);
+  // DOM helpers
+  function createFeedbackButton(data, text, verdict) {
+    return element("a", "button ai-fire-feedback-button", {
+      text: text,
+      click: function () {
+        feedback(data, verdict);
+      }
+    });
+  }
+
+  // Wrapper to create a new element with a specified class.
+  function element(tagName, cssClass, options) {
+    options = options || {};
+    options.class = cssClass;
+    return $("<" + tagName + "/>", options);
+  }
+
+  // Handle CSS injection
+  function injectCSS(wip) {
+    if (wip) {
+      // This userscript is still work in progress. It's probably broken, useless or otherwise dangerous.
+      $.get(
+        "https://api.github.com/repos/Charcoal-SE/Userscripts/commits/FIRE",
+        function (commitData) {
+          var css = window.document.createElement("link");
+          css.rel = "stylesheet";
+          css.href = "https://cdn.rawgit.com/Charcoal-SE/Userscripts/" + commitData.sha + "/fire/fire.css";
+          document.head.appendChild(css);
         }
-      });
-
-      $fire.prepend(fireButton);
+      );
+    } else {
+      // Release
+      // Inject CSS
+      var css = window.document.createElement("link");
+      css.rel = "stylesheet";
+      css.href = "//charcoal-se.org/userscripts/fire.css"; // "cdn.rawgit.com/Charcoal-SE/userscripts/master/fire/fire.css"
+      document.head.appendChild(css);
     }
-  };
-
-  autoflagging.decorate.fire.location = "after";
+  }
 })();
