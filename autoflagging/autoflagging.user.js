27d42af87a4f396bdafe66727067afb1671dbf40
Cerbrus <michiel1900@gmail.com>
2017-03-18 13:43:55 +0100

    AIM version bump

--- autoflagging/autoflagging.user.js
+++ autoflagging/autoflagging.user.js
@@ -7,7 +7,7 @@
 // @contributor angussidney
 // @contributor ArtOfCode
 // @contributor Cerbrus
-// @version     0.13.1
+// @version     0.13.2
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.user.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
