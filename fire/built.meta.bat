a2c2ded45bb8b28f14a2b6bbb7f33bb11375cc8d
Cerbrus <michiel1900@gmail.com>
2017-03-20 17:08:08 +0100

    Remove whitespace.

--- fire/built.meta.bat
+++ fire/built.meta.bat
@@ -8,7 +8,7 @@ SET /A linecount=0
 
 FOR /F "delims=" %%A IN (fire.user.js) DO ( 
   IF !linecount! GEQ %maxlines% GOTO ExitLoop
-  ECHO %%A >> fire.meta.js
+  ECHO %%A>>fire.meta.js
   SET /A linecount+=1
 )
 
--- fire/fire.meta.js
+++ fire/fire.meta.js
@@ -1,15 +1,15 @@
-// ==UserScript== 
-// @name        Flag Instantly, Rapidly, Effortlessly 
-// @namespace   https://github.com/Charcoal-SE/ 
-// @description FIRE adds a button to SmokeDetector reports that allows you to provide feedback & flag, all from chat. 
-// @author      Cerbrus 
-// @attribution Michiel Dommerholt (https://github.com/Cerbrus) 
-// @version     0.9.10 
-// @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js 
-// @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js 
-// @supportURL  https://github.com/Charcoal-SE/Userscripts/issues 
-// @match       *://chat.stackexchange.com/rooms/11540/charcoal-hq* 
-// @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers* 
-// @match       *://chat.meta.stackexchange.com/rooms/89/tavern-on-the-meta* 
-// @grant       none 
-// ==/UserScript== 
+// ==UserScript==
+// @name        Flag Instantly, Rapidly, Effortlessly
+// @namespace   https://github.com/Charcoal-SE/
+// @description FIRE adds a button to SmokeDetector reports that allows you to provide feedback & flag, all from chat.
+// @author      Cerbrus
+// @attribution Michiel Dommerholt (https://github.com/Cerbrus)
+// @version     0.9.10
+// @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js
+// @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js
+// @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
+// @match       *://chat.stackexchange.com/rooms/11540/charcoal-hq*
+// @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers*
+// @match       *://chat.meta.stackexchange.com/rooms/89/tavern-on-the-meta*
+// @grant       none
+// ==/UserScript==
