0904691a9cda1966e8c036c53870dab925de8937
Cerbrus <michiel1900@gmail.com>
2017-03-21 02:26:32 +0100

    Description

--- extractUserScriptMeta.sh
+++ extractUserScriptMeta.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Charcoal-SE/userscripts build script. Scripts should each have their own directory; if they wish to customise any
-# repo-wide settings, they should also have their own package.json in the directory.
+# Generate <script>.meta.js files containing only the script's metadata,
+# so the whole script doesn't have to be downloaded to check for updates.
+# Example use:
+# // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js
+# // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js
 
 find ./ -name "*user.js" -type f | while read file; do
   newName=${file/.user./.meta.}
