26425df0df9fa17c4969647352d15f0c873d1b2f
Cerbrus <michiel1900@gmail.com>
2017-03-21 02:24:54 +0100

    Generated from extractUserScriptMeta.sh

---
+++ aeks/aeks.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Automatically Enable Keyboard Shortcuts
+// @namespace   https://github.com/Charcoal-SE/
+// @description Automatically enable keyboard shortcuts for any SE site you're on. This may require a reload for the setting to take effect.
+// @author      Cerbrus
+// @attribution Michiel Dommerholt (https://github.com/Cerbrus)
+// @version     1.1.0
+// @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/aeks/aeks.meta.js
+// @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/aeks/aeks.user.js
+// @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
+// @match       *://*.stackexchange.com/*
+// @match       *://*.stackoverflow.com/*
+// @match       *://*.superuser.com/*
+// @match       *://*.serverfault.com/*
+// @match       *://*.askubuntu.com/*
+// @match       *://*.stackapps.com/*
+// @match       *://*.mathoverflow.net/*
+// @exclude     *://chat.stackexchange.com/*
+// @exclude     *://chat.meta.stackexchange.com/*
+// @exclude     *://chat.stackoverflow.com/*
+// @exclude     *://blog.stackoverflow.com/*
+// @exclude     *://*.area51.stackexchange.com/*
+// @grant       none
+// ==/UserScript==
