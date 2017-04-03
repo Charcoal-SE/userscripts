e54176693e0ddb0fe9453fa10e1c40e197e20f85
Cerbrus <michiel1900@gmail.com>
2017-04-03 18:40:29 +0200

    Adding FIRE / AIM support to The Spam Blot

--- autoflagging/autoflagging.meta.js
+++ autoflagging/autoflagging.meta.js
@@ -7,13 +7,14 @@
 // @contributor angussidney
 // @contributor ArtOfCode
 // @contributor Cerbrus
-// @version     0.13.6
+// @version     0.13.7
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.meta.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
 // @match       *://chat.stackexchange.com/rooms/11540/charcoal-hq*
 // @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers*
 // @match       *://chat.meta.stackexchange.com/rooms/89/tavern-on-the-meta*
+// @match       *://chat.stackexchange.com/rooms/56223/the-spam-blot*
 // @require     https://cdn.rawgit.com/joewalnes/reconnecting-websocket/f8055b77ba75e5d564ffb50d20a483bdd7edccdf/reconnecting-websocket.min.js
 // @grant       none
 // ==/UserScript==
