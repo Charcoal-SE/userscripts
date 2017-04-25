7d391b32d34b3c4b177cf4434e84c57524eea51f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2017-04-25 19:29:35 +0200

    Forgot to prepare userscript header for release

--- spamtracker/spamtracker.user.js
+++ spamtracker/spamtracker.user.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         SpamtrackerReboot
-// @version      0.9
+// @version      1.0
 // @description  Rewrite of the spamtracker project, this userscript will notify you using sound and a notification if a new spam post has been posted in any smoke detector supported rooms
 // @author       Ferrybig
 // @match        *://chat.meta.stackexchange.com/*
 // @match        *://chat.stackexchange.com/*
 // @match        *://chat.stackoverflow.com/*
 // @run-at       document-end
+// @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/spamtracker/spamtracker.meta.js
+// @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/spamtracker/spamtracker.user.js
+// @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
 // @require      https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js#sha512=1ac1502c5a6774e6e7d3c77dd90d863f745371cd936d8a1620ab1c4a21173ffccfd327e435395df6658779ea87baad3b5ff84bf195110c7bc3187112ee820917
 // @resource     DataTablesCSS https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css#sha256=f99d6b61adf2b3939d64d51c9391bb941bdbf00d773ab630bdff9df0f7c46874
 // @resource     DataTablesSortAsc https://cdn.datatables.net/1.10.13/images/sort_asc.png#sha256=595704c3f3cf4cb65c7d9c8508a99e7480e150095473faed31a07c21b13389b8
