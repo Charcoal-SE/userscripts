1ab3255e110660d890cc76ebc5fec1219d8360c8
Cerbrus <michiel1900@gmail.com>
2017-05-16 12:25:03 +0200

    Fixing FIRE commit messup

--- fire/fire.meta.js
+++ fire/fire.meta.js
@@ -4,7 +4,7 @@
 // @description FIRE adds a button to SmokeDetector reports that allows you to provide feedback & flag, all from chat.
 // @author      Cerbrus
 // @attribution Michiel Dommerholt (https://github.com/Cerbrus)
-// @version     1.0.8
+// @version     1.0.10
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
@@ -12,5 +12,6 @@
 // @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers*
 // @match       *://chat.meta.stackexchange.com/rooms/89/tavern-on-the-meta*
 // @match       *://chat.stackexchange.com/rooms/56223/the-spam-blot*
+// @match       *://chat.stackoverflow.com/rooms/111347/sobotics*
 // @grant       none
 // ==/UserScript==
--- fire/fire.user.js
+++ fire/fire.user.js
@@ -4,7 +4,7 @@
 // @description FIRE adds a button to SmokeDetector reports that allows you to provide feedback & flag, all from chat.
 // @author      Cerbrus
 // @attribution Michiel Dommerholt (https://github.com/Cerbrus)
-// @version     1.0.8
+// @version     1.0.10
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
@@ -12,6 +12,7 @@
 // @match       *://chat.stackoverflow.com/rooms/41570/so-close-vote-reviewers*
 // @match       *://chat.meta.stackexchange.com/rooms/89/tavern-on-the-meta*
 // @match       *://chat.stackexchange.com/rooms/56223/the-spam-blot*
+// @match       *://chat.stackoverflow.com/rooms/111347/sobotics*
 // @grant       none
 // ==/UserScript==
 
@@ -277,9 +278,9 @@
       ({user_name}) => user_name === fire.chatUser.name
     );
 
-    const match = data.link.match(/\d+/);
-    if (match && match[0])
-      [data.post_id] = match;
+    const match = data.link.match(/.*\/(\d+)/);
+    if (match && match[1])
+      [, data.post_id] = match;
 
     fire.reportCache[data.link] = data; // Store the data
 
