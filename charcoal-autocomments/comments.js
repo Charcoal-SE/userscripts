131d66c9ba42859706bccfd74c18a0e37b999d7f
CalvT <calvin@jstomkins.com>
2017-05-19 14:10:46 -0300

    Updates

--- charcoal-autocomments/comments.js
+++ charcoal-autocomments/comments.js
@@ -6,15 +6,23 @@ callback(
       description: "Please do not vandalize your posts. Once you've posted a question, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
     },
     {
+      name: "[Q] Rude",
+      description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please [Be Nice](https://$SITEURL$/help/be-nice): *Avoid vulgar terms and anything sexually suggestive*"
+    },
+    {
       name: "[A] No Disclosure",
-      description: "Please note if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you mention it to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
+      description: "Please note if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
     },
     {
       name: "[A] Rude",
-      description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please Be Nice: *Avoid vulgar terms and anything sexually suggestive*"
+      description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please [Be Nice](https://$SITEURL$/help/be-nice): *Avoid vulgar terms and anything sexually suggestive*"
     },
     {
-      name: "[A] Nothing but a URL (and isn't spam)",
+      name: "[A] Nothing but a URL, and no disclosure",
+      description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here, and provide the link for reference. Also please note that if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
+    },
+    {
+      name: "[A] Nothing but a URL and has disclosure",
       description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here, and provide the link for reference."
     },
     {
