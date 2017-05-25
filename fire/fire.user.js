b6bc428804d20108e7a5be95791e48dc15579cff
Jed Fox <git@twopointzero.us>
2017-05-25 11:16:18 -0400

    Pacify prefer-const

--- fire/fire.user.js
+++ fire/fire.user.js
@@ -247,6 +247,7 @@
         if (response.items.length <= 0)
           toastr.info('No metasmoke reports found.');
 
+        // eslint-disable-next-line prefer-const
         for (let item of response.items)
           parseDataForReport(item, false, null, true);
       }
@@ -335,6 +336,7 @@
         'sites',
         parameters,
         ({items}) => {
+          // eslint-disable-next-line prefer-const
           for (let item of items)
             sites[item.api_site_parameter] = item;
 
@@ -1242,6 +1244,7 @@
     const selected = fire.userData.toastrPosition;
     const positionSelect = _('select', 'fire-position-select', {change: toastrPositionChangeHandler});
 
+    // eslint-disable-next-line prefer-const
     for (let val of toastrClasses) {
       positionSelect.append(
         _('option', {
@@ -2105,6 +2108,7 @@
       fire.info('Debug mode enabled.');
 
     const data = fire.userData;
+    // eslint-disable-next-line prefer-const
     for (let key in defaultStorage) {
       if (hOP(defaultStorage, key) && !hOP(data, key))
         data[key] = defaultStorage[key];
