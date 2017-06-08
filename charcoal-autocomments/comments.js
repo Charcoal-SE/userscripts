7481e7c2356a60eb35bf61536f1c5adcc5567575
CalvT <calvin@jstomkins.com>
2017-06-08 10:02:38 -0300

    Removed duplicates between questions and answers

--- charcoal-autocomments/comments.js
+++ charcoal-autocomments/comments.js
@@ -2,20 +2,16 @@
 callback(
   [
     {
-      name: "[Q] Vandalism",
-      description: "Please do not vandalize your posts. Once you've posted a question, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
+      name: "[Q, A] Vandalism",
+      description: "Please do not vandalize your posts. Once you have submitted a post, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
     },
     {
-      name: "[Q] Rude",
+      name: "[Q, A] Rude",
       description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please [Be Nice](https://$SITEURL$/help/be-nice): *Avoid vulgar terms and anything sexually suggestive*"
     },
     {
       name: "[A] No Disclosure",
       description: "Please note if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
-    },
-    {
-      name: "[A] Rude",
-      description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please [Be Nice](https://$SITEURL$/help/be-nice): *Avoid vulgar terms and anything sexually suggestive*"
     },
     {
       name: "[A] Nothing but a URL, and no disclosure",
@@ -28,9 +24,5 @@ callback(
     {
       name: "[A] Requests to OP for further information",
       description: "This is really a comment, not an answer. With a bit more rep, [you will be able to post comments](https://$SITEURL$/privileges/comment). For the moment I've added the comment for you, and I'm flagging this post for deletion."
-    },
-    {
-      name: "[A] Vandalism",
-      description: "Please do not vandalize your posts. Once you've posted an answer, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
     }
   ]);
