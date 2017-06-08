a572bac66d09e4e8ce96915bf765e4806abba9d6
CalvT <calvin@jstomkins.com>
2017-06-08 10:16:01 -0300

    Make Grammarly happy

--- charcoal-autocomments/comments.js
+++ charcoal-autocomments/comments.js
@@ -2,24 +2,24 @@
 callback(
   [
     {
-      name: "[Q, A] Vandalism",
-      description: "Please do not vandalize your posts. Once you have submitted a post, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
+      name: "[Q,A] Vandalism",
+      description: "Please do not vandalise your posts. Once you have submitted a post, you have licensed the content to the $SITENAME$ community at large (under the CC-by-SA license). If you would like to disassociate this post from your account, see [What is the proper route for a disassociation request](https://meta.stackoverflow.com/questions/323395)?"
     },
     {
-      name: "[Q, A] Rude",
+      name: "[Q,A] Rude",
       description: "**Warning - please do not use obscenities in your posts.** Note that other users may have flagged your post as **rude or abusive** leading to possible rep loss or suspension. Please [Be Nice](https://$SITEURL$/help/be-nice): *Avoid vulgar terms and anything sexually suggestive*"
     },
     {
       name: "[A] No Disclosure",
-      description: "Please note if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
+      description: "Please note if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise, your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
     },
     {
       name: "[A] Nothing but a URL, and no disclosure",
-      description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here, and provide the link for reference. Also please note that if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
+      description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here and provide the link for reference. Also please note that if you want to promote your own product/blog you **must disclose your affiliation in the answer**, otherwise your answer may be flagged as spam. If you are not affiliated with the site, I recommend you say so to prevent this. Please read [How to not be a spammer](https://$SITEURL$/help/promotion)"
     },
     {
       name: "[A] Nothing but a URL and has disclosure",
-      description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here, and provide the link for reference."
+      description: "While this may theoretically answer the question, [it would be preferable](https://meta.stackexchange.com/questions/8231) to include the essential parts of the answer here and provide the link for reference."
     },
     {
       name: "[A] Requests to OP for further information",
