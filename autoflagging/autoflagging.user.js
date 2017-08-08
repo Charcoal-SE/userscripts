8d5967b45fa515e4f2d489e9275b8c0e9b7ed8c6
Jed Fox <git@twopointzero.us>
2017-08-08 08:59:38 -0400

    AIM: Fix ReconnectingWebSocket download link
    I have no idea why it broke.

--- autoflagging/autoflagging.user.js
+++ autoflagging/autoflagging.user.js
@@ -7,7 +7,7 @@
 // @contributor angussidney
 // @contributor ArtOfCode
 // @contributor Cerbrus
-// @version     0.15.1
+// @version     0.16.0
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.meta.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/autoflagging/autoflagging.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
@@ -17,7 +17,7 @@
 // @match       *://chat.stackexchange.com/rooms/56223/the-spam-blot*
 // @match       *://chat.stackoverflow.com/rooms/111347/sobotics*
 // @match       *://chat.stackexchange.com/rooms/201/ask-ubuntu-general-room*
-// @require     https://cdn.rawgit.com/joewalnes/reconnecting-websocket/f80355b77ba75e5d564ffb50d20a483bdd7edccdf/reconnecting-websocket.min.js
+// @require     https://cdn.rawgit.com/joewalnes/reconnecting-websocket/fd7c819bb15eeee3452c17e317c0a3664c442965/reconnecting-websocket.min.js
 // @require     https://charcoal-se.org/userscripts/vendor/debug.min.js
 // @grant       none
 // ==/UserScript==
