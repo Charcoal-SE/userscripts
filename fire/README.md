1b91094f86c89e6542091693da159880d9932b87
Cerbrus <michiel1900@gmail.com>
2017-09-09 12:47:43 +0200

    Bugfix: FireFox still has issues loading the current user.
    1.0.18 Bugfix: FireFox still has issues loading the current user.

--- fire/README.md
+++ fire/README.md
@@ -59,6 +59,7 @@ To open the reported post on it's site:
 
 |1.0    ||
 | ---   |---
+|1.0.18 |Bugfix: FireFox still has issues loading the current user.
 |1.0.17 |Bugfix: Escape style tags in posts.
 |1.0.16 |Bugfix: Escape script tags in posts.
 |1.0.15 |Bugfix: Escape HTML in report titles.
--- fire/fire.user.js
+++ fire/fire.user.js
@@ -4,7 +4,7 @@
 // @description FIRE adds a button to SmokeDetector reports that allows you to provide feedback & flag, all from chat.
 // @author      Cerbrus
 // @attribution Michiel Dommerholt (https://github.com/Cerbrus)
-// @version     1.0.17
+// @version     1.0.18
 // @updateURL   https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.meta.js
 // @downloadURL https://raw.githubusercontent.com/Charcoal-SE/Userscripts/master/fire/fire.user.js
 // @supportURL  https://github.com/Charcoal-SE/Userscripts/issues
@@ -2163,7 +2163,7 @@
 
           fire.log('Current user found.');
         });
-    });
+    }, 250); // Maybe this is enough?
   }
 
   /**
