367aa261c6888902c1c0b92d3662c59c41546204
SulphurDioxide <git@adhenderson.co.uk>
2018-02-17 15:43:54 +0000

    Fixes single to double quotes

--- spam/SmokeyPings.user.js
+++ spam/SmokeyPings.user.js
@@ -11,18 +11,18 @@
 
 // Set the constant by passing in an object and then selecting which key to use based on the current host:
 const smokeyID = {
-  'chat.stackexchange.com':120914,
-  'chat.stackoverflow.com':3735529,
+  "chat.stackexchange.com":120914,
+  "chat.stackoverflow.com":3735529,
 }[window.location.host];
 
 // If there is no sound set, set the default one:
-if(localStorage.getItem('SPAM-settings') == null){
+if(localStorage.getItem("SPAM-settings") == null){
   // Set the default value:
   var defaultSpamSettings = {
-    'notificationSound':'default',
+    "notificationSound":"default",
   };
   
-  localStorage.setItem('SPAM-settings', JSON.stringify(defaultSpamSettings));
+  localStorage.setItem("SPAM-settings", JSON.stringify(defaultSpamSettings));
 }
 
 /*
@@ -44,12 +44,12 @@ $(document).ready(function(){
   CHAT.addEventHandlerHook(chatMessageRecieved);
   
   // Add the SPAM link to change the settings:
-  $('#sidebar-menu').append("| <a id='spamOptions' href='#' onclick='return false'>SPAM</a>");
-  $('head').append("<style> .spamSettings { position: fixed; z-index: 50; padding: 10px; border: 1px solid #aaa; width: 200px; font-size: 11px; color: #444; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px; background: rgba(255,255,255,0.95); filter: alpha(opacity=95); -webkit-box-shadow: 0 1px 15px #9c9c9c; -moz-box-shadow: 0 1px 15px #9c9c9c; box-shadow: 0 1px 15px #9c9c9c; overflow: hidden; } </style>");
+  $("#sidebar-menu").append("| <a id='spamOptions' href='#' onclick='return false'>SPAM</a>");
+  $("head").append("<style> .spamSettings { position: fixed; z-index: 50; padding: 10px; border: 1px solid #aaa; width: 200px; font-size: 11px; color: #444; -moz-border-radius: 5px; -webkit-border-radius: 5px; border-radius: 5px; background: rgba(255,255,255,0.95); filter: alpha(opacity=95); -webkit-box-shadow: 0 1px 15px #9c9c9c; -moz-box-shadow: 0 1px 15px #9c9c9c; box-shadow: 0 1px 15px #9c9c9c; overflow: hidden; } </style>");
   
-  $('#jplayer').append('<audio id="jp_audio_clavinova" preload="metadata" src="https://adhenderson.co.uk/sounds/clavinova.mp3"></audio><audio id="jp_audio_vibraphone" preload="metadata" src="https://adhenderson.co.uk/sounds/vibraphone.mp3"></audio><audio id="jp_audio_piano" preload="metadata" src="https://adhenderson.co.uk/sounds/piano.mp3"></audio>');
+  $("#jplayer").append("<audio id='jp_audio_clavinova' preload='metadata' src='https://adhenderson.co.uk/sounds/clavinova.mp3'></audio><audio id='jp_audio_vibraphone' preload='metadata' src='https://adhenderson.co.uk/sounds/vibraphone.mp3'></audio><audio id='jp_audio_piano' preload='metadata' src='https://adhenderson.co.uk/sounds/piano.mp3'></audio>");
   
-  $('#spamOptions').click(function(){
+  $("#spamOptions").click(function(){
     toggleSpamOptions();
   });
 });
@@ -89,54 +89,54 @@ function chatMessageRecieved({event_type, user_id, content}){
 function toggleSpamOptions(){
   var spamOptionsMenu = "<div id='spamOptionsMenu' class='spamSettings'><div style='position: absolute; right: 10px;'><a id='closeSpamOptions' href='#' onclick='return false;'>Close</a></div><h2>Spam Options</h2><strong>Notification Sound</strong>:<div><select id='spamSoundSelect'><option id='defaultOption' class='SPAM-option' value='default'>Default</option><option id='pianoOption' class='SPAM-option' value='piano'>Piano</option><option id='clavinovaOption' class='SPAM-option' value='clavinova'>Clavinova</option><option id='vibraphoneOption' class='SPAM-option' value='vibraphone'>Vibraphone</option></select></div> </div>";
     
-  if($('#spamOptionsMenu').length < 1){
-    $('#spamOptions').after(spamOptionsMenu);
+  if($("#spamOptionsMenu").length < 1){
+    $("#spamOptions").after(spamOptionsMenu);
     
-    var storedSound = JSON.parse(localStorage.getItem('SPAM-settings')).notificationSound;
-    $('#'+storedSound+'Option').attr('selected', true);
+    var storedSound = JSON.parse(localStorage.getItem("SPAM-settings")).notificationSound;
+    $("#"+storedSound+"Option").attr("selected", true);
     
-    $('#closeSpamOptions').click(function(){
+    $("#closeSpamOptions").click(function(){
       toggleSpamOptions();
     });
-    $('#spamSoundSelect').change(function(){
+    $("#spamSoundSelect").change(function(){
       playSpamSound(this.value);
       setSpamSound(this.value);
     });
     
   } else {
     // Get rid of the menu if it already exists:
-    $('#spamOptionsMenu').remove();
+    $("#spamOptionsMenu").remove();
   }
 }
 
 function setSpamSound(sound){
   // Current stored settings:
-  var currentStoredSettings = JSON.parse(localStorage.getItem('SPAM-settings'));
+  var currentStoredSettings = JSON.parse(localStorage.getItem("SPAM-settings"));
   
   currentStoredSettings.notificationSound = sound;
   
-  localStorage.setItem('SPAM-settings', JSON.stringify(currentStoredSettings));
+  localStorage.setItem("SPAM-settings", JSON.stringify(currentStoredSettings));
 
-  $('.SPAM-option').attr('selected', false);
-  $('#'+sound+'Option').attr('selected', true);
+  $(".SPAM-option").attr("selected", false);
+  $("#"+sound+"Option").attr("selected", true);
 }
 
 function playSpamSound(sound){
   const soundMap = {
-    'default':'jp_audio_0',
-    'clavinova':'jp_audio_clavinova',
-    'piano':'jp_audio_piano',
-    'vibraphone':'jp_audio_vibraphone',
+    "default":"jp_audio_0",
+    "clavinova":"jp_audio_clavinova",
+    "piano":"jp_audio_piano",
+    "vibraphone":"jp_audio_vibraphone",
   };
   
-  if(typeof sound == 'undefined'){
+  if(typeof sound == "undefined"){
     // Play the stored sound:
-    var storedSound = JSON.parse(localStorage.getItem('SPAM-settings')).notificationSound;
+    var storedSound = JSON.parse(localStorage.getItem("SPAM-settings")).notificationSound;
     
-    $('#'+soundMap[storedSound])[0].play();
+    $("#"+soundMap[storedSound])[0].play();
     
   } else {
     // Play the sound specified:
-    $('#'+soundMap[sound])[0].play();
+    $("#"+soundMap[sound])[0].play();
   }
 }
