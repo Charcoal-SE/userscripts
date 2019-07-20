8178a19520dbbe75675027027722c5b340b3e2fa
Makyen <wp.makyen@gmail.com>
2019-07-19 21:09:58 -0700

    Change build.sh & package.json so build.sh can be used in development, not just CI. Use "ignores" in /package.json to ignore directories with their own package.json, rather than have build.sh move directories around. Moving directories around results in leaving the repository in a corrupted state, if there is a failure, which is a common thing in development.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Charcoal-SE/userscripts build script. Scripts should each have their own directory; if they wish to customise any
-# repo-wide settings, they should also have their own package.json in the directory.
+# Charcoal-SE/userscripts build script. Scripts should each have their own directory.
+# If a script desires to customise any repo-wide settings, it should have its own
+# package.json in the script's directory and the script's directory should be added
+# to "ignores" in /package.json.
 
 overridden=""
 
@@ -12,13 +14,12 @@ do
   if [ -f $dir"package.json" ]
   then
     echo "Overridden package.json: $dir"
-    echo "Ignoring: $dir"
-    mv $dir "/tmp/$dir"
     overridden=$overridden";;$dir"
   fi
 done
 
 echo "Building main project..."
+npm install
 npm test
 ecode=$?
 if [ "$ecode" != "0" ]
@@ -31,7 +32,6 @@ dirs=(${overridden//;;/ })
 for dir in ${dirs[@]} ;
 do
   echo "Building overridden: $dir"
-  mv "/tmp/$dir" .
   cd $dir
   npm install
   npm test
